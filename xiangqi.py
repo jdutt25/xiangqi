9dbc230ba7d3518a7b47fc76fb818d0037ca2565
Jess <jessjdutton@gmail.com>
2020-07-12 11:05:46 -0700

    updated board and unittests

--- xiangqi.py
+++ xiangqi.py
@@ -20,7 +20,7 @@ class XiangqiGame:
         """
         self._turn = "R"
         self._game_state = "UNFINISHED"
-        self._game_board = board.Board()             # calls the Board class
+        self._game_board = board.Board()       # calls the Board class
         self._piece = piece.Piece(self._game_board.get_board)  # calls the Piece class, passing to it the current game board
         self._check_status = False
         self._checkmate_status = False
@@ -263,8 +263,8 @@ class XiangqiGame:
 
             while len(self._piece.get_legal_moves()) > 0:
                 if move_to1 == self._piece.get_legal_moves()[0] and move_to2 == self._piece.get_legal_moves()[1]:
-                    replace = "  "      # make move
-                    self._game_board.set_board(move_from1, move_from2, move_to1, move_to2, value, replace)
+                    replace = "  "
+                    self._game_board.set_board(move_from1, move_from2, move_to1, move_to2, value, replace) # move the piece on the board and update move from space to empty with replace
                     if self.is_in_check(self.get_full_color()) is False:  # player cannot put themselves in check
                         self.set_turn()  # next player's turn, first check to see if they're in check/checkmate
                         if self.get_full_color() == "red":
@@ -275,6 +275,7 @@ class XiangqiGame:
                         self.checkmate(self.get_full_color())         # check to see if the other color is in checkmate
                         return True
                     else:
+                        print("you cannot make a move that puts yourself in check")
                         replace = move_to  # put piece back
                         self._game_board.set_board(move_to1, move_to2, move_from1, move_from2, value, replace)
                         del self._piece.get_legal_moves()[0]
@@ -284,9 +285,57 @@ class XiangqiGame:
                     del self._piece.get_legal_moves()[0]
             return False
         else:
+            print("illegal move")
             return False
 
 
-x = XiangqiGame()
-x.make_move("e4","e5")
-x._game_board.print_board()
+
+def main():
+   game = XiangqiGame()
+   game.make_move("a4", "a5")
+  # game.get_game_board().print_board()
+   game.make_move("b10", "c8")
+   game.make_move("b3", "b5")
+   game.make_move("a10", "a9")
+   game.make_move("b5", "i5")
+   game.make_move("h10", "i8")
+   game.make_move("i5", "i8")
+   game.make_move("i10", "i8")
+   game.make_move("h3", "h7")
+   game.get_game_board().print_board()
+   game.make_move("a9", "d9")
+   game.make_move("h7", "e7")
+   game.make_move("c8", "e7")
+   game.make_move("e1", "e2")
+   game.make_move("d9", "d1")
+   game.make_move("c1", "e3")
+   game.make_move("d1", "f1")
+   game.make_move("g1", "i3")
+   game.make_move("c10", "e8")
+   game.make_move("e2", "d2")
+   game.make_move("h8", "g8")
+   game.make_move("b1", "c3")
+   game.make_move("f1", "a1")
+   game.make_move("c3", "b1")
+   game.make_move("a1", "b1")
+   game.make_move("h1", "g3")
+   game.make_move("b1", "i1")
+   game.make_move("g3", "e2")
+   game.make_move("i1", "i3")
+   game.make_move("e3", "g5")
+   game.make_move("i3", "f3")
+   game.make_move("e2", "g3")
+   game.make_move("f3", "g3")
+   game.make_move("d2", "d1")
+   game.make_move("g3", "g1")
+   game.make_move("d1", "d2")
+   game.make_move("g1", "g4")
+   game.make_move("c4", "c5")
+   game.make_move("g8", "g5")
+   game.make_move("e4", "e5")
+   game.make_move("g4", "d4")
+   print(game.is_in_check("red"))
+  # print(game.make_move("c5", "c6")) THIS IS A PROBLEM
+
+
+if __name__ == '__main__': main()
--- xiangqiTests.py
+++ xiangqiTests.py
@@ -2,16 +2,61 @@
 # Description: unit tests for Xianqi
 
 import unittest
-import xiangqi, board, piece
+from xiangqi import XiangqiGame
 
 class TestXianqi(unittest.TestCase):
     """
     defines unit tests for Xianqi
     """
+    def setup(self):
+        pass
 
     def test1(self):
         """
         tests black putting red in check, red attempting to make a move that does not take it out of check
         """
-        xiangqi = xianqi()
-        self.assertEqual(xiangqi.make_move("a4", "a5"), True)
+        game = XiangqiGame()
+        game.make_move("a4", "a5")
+        game.make_move("b10", "c8")
+        game.make_move("b3", "b5")
+        game.make_move("a10", "a9")
+        game.make_move("b5", "i5")
+        game.make_move("h10", "i8")
+        game.make_move("i5", "i8")
+        game.make_move("i10", "i8")
+        game.make_move("h3", "h7")
+        game.make_move("a9", "d9")
+        game.make_move("h7", "e7")
+        game.make_move("c8", "e7")
+        game.make_move("e1", "e2")
+        game.make_move("d9", "d1")
+        game.make_move("c1", "e3")
+        game.make_move("d1", "f1")
+        game.make_move("g1", "i3")
+        game.make_move("c10", "e8")
+        game.make_move("e2", "d2")
+        game.make_move("h8", "g8")
+        game.make_move("b1", "c3")
+        game.make_move("f1", "a1")
+        game.make_move("c3", "b1")
+        game.make_move("a1", "b1")
+        game.make_move("h1", "g3")
+        game.make_move("b1", "i1")
+        game.make_move("g3", "e2")
+        game.make_move("i1", "i3")
+        game.make_move("e3", "g5")
+        game.make_move("i3", "f3")
+        game.make_move("e2", "g3")
+        game.make_move("f3", "g3")
+        game.make_move("d2", "d1")
+        game.make_move("g3", "g1")
+        game.make_move("d1", "d2")
+        game.make_move("g1", "g4")
+        game.make_move("c4", "c5")
+        game.make_move("g8", "g5")
+        game.make_move("e4", "e5")
+        game.make_move("g4", "d4")
+        self.assertEqual(game.is_in_check("black"), False)
+        self.assertEqual(game.is_in_check("red"), True)
+
+
